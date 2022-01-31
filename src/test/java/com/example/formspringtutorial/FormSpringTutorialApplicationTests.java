bfb5b212ec6c34309c947fe14b658f8d1bbc62c1
Iaskara Pimentel <iaiadivi@gmail.com>
2022-01-31 10:22:00 +0100

    first commit

---
+++ src/test/java/com/example/formspringtutorial/FormSpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formspringtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormSpringTutorialApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
